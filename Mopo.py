07a550e716aaaba1ea9bf1fb8bb8be898de3e79d
Omar Usmani <omar.usmani@tno.nl>
2024-12-19 09:27:54 +0100

    Mopo case sudy first run

--- Mopo.py
+++ Mopo.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import typing as ty
 
+import pandas as pd
 from ChaProEV import ChaProEV
 from ETS_CookBook import ETS_CookBook as cook
 
@@ -28,9 +29,41 @@ def copy_select_files(case_name: str) -> None:
                             )
 
 
+def rename_files_with_country_codes(case_name: str) -> None:
+    select_output_folder: str = f'select_output/{case_name}/'
+    country_codes: pd.DataFrame = pd.read_csv('country_codes.csv').set_index(
+        'Country'
+    )
+    print(country_codes.loc['United_Kingdom']['Code'])
+    for country_name in country_codes.index:
+        country_files: ty.List[str] = [
+            data_file
+            for data_file in os.listdir(select_output_folder)
+            if data_file.startswith(country_name)
+        ]
+        country_files_with_code: ty.List[str] = [
+            country_file.replace(
+                country_name, country_codes.loc[country_name]['Code']
+            )
+            for country_file in country_files
+        ]
+        for country_file, country_file_with_code in zip(
+            country_files, country_files_with_code
+        ):
+            os.rename(
+                f'{select_output_folder}/{country_file}',
+                f'{select_output_folder}/{country_file_with_code}',
+            )
+
+
 if __name__ == '__main__':
     case_name: str = 'Mopo'
     ChaProEV.run_ChaProEV(case_name)
 
     copy_select_files(case_name)
+    rename_files_with_country_codes(case_name)
+    print(
+        ' line 81 in car home parking needs an if sessions and '
+        'if generate profiles'
+    )
 # test use profile from net * effective effiicency (replace errors by zeros)
